1d1e0d0a04de8b965545eb173c7a0f883fb46567
sanglq3 <sanglq3@vng.com.vn>
2023-12-29 11:34:33 +0700

    Update simple item

--- src/components/Pano.tsx
+++ src/components/Pano.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useRef } from 'react';
 
+function createInfoHotspotElement(hotspot: any) {
+  var wrapper = document.createElement("div");
+  wrapper.classList.add("hotspot");
+  wrapper.classList.add("w-[100px]");
+  wrapper.classList.add("h-[100px]");
+  var icon = document.createElement("img");
+  icon.src = "/react.png";
+  icon.height = 100;
+  icon.width = 100;
+  wrapper.appendChild(icon);
+  return wrapper;
+}
+
 function Pano({ data }: any) {
   const panoRef = useRef(null);
   useEffect(() => {
@@ -40,11 +53,18 @@ function Pano({ data }: any) {
           });
 
           var imgHotspot = document.createElement("img");
-          imgHotspot.src = "/next.svg";
+          imgHotspot.src = "/react.png";
           imgHotspot.classList.add("hotspot");
 
           var position = { yaw: Math.PI / 4, pitch: Math.PI / 8 };
           scene.hotspotContainer().createHotspot(imgHotspot, position);
+
+          // Create info hotspots.
+          data.infoHotspots.forEach(function (hotspot: any) {
+            var element = createInfoHotspotElement(hotspot);
+            scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
+          });
+
 
           return {
             data: data,
@@ -57,8 +77,6 @@ function Pano({ data }: any) {
       });
     }
   }, [data]);
-
-  console.log(panoScenes);
 
   return (
     <div className='pano-container h-[1000px] w-[100vw]' ref={panoRef}>
